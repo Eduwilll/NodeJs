1abebb3b9ef39b0bbd937c617921b7cd66d56904
Eduwill <eduwill100@gmail.com>
2022-08-01 01:25:02 -0300

    first commit

---
+++ interação com banco de dados/find-produtos.js
@@ -0,0 +1,23 @@
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://localhost:27017";
+const cliente = new MongoClient(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+async function listar() {
+    try {
+        await cliente.connect();
+        console.log("Conectado!");
+        const banco = cliente.db("test");
+        var cursor = banco.collection("produtos").find();
+        await cursor.forEach(function (doc) {
+        console.log("---");
+        console.log("Código: " + doc.codigo);
+        console.log("Descrição: " + doc.descricao);
+        console.log("Preço: " + doc.preco);
+        });
+    } finally {
+    await cliente.close();
+    }
+}
+listar();
